cb32bd9280d7234002c2576bebb89bf583775671
Baraa <124203201+0baraa@users.noreply.github.com>
2025-06-12 18:29:34 +0100

    Ex9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  const bothNumbers = typeof start === 'number' && typeof end === 'number';
+  const bothIntegers = Number.isInteger(start) && Number.isInteger(end);
+  const bothNonNegative = start >= 0 && end >= 0;
 
+  if (!bothNumbers || !bothIntegers || !bothNonNegative) {
+    return 'ERROR';
+  }
+
+  if (start > end) [start, end] = [end, start];
+
+  let sum = 0;
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
