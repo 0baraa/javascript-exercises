85162d75d01e0730c175eae1a8dd1b76f87da348
Baraa <124203201+0baraa@users.noreply.github.com>
2025-06-15 11:57:50 +0100

    12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(op1, op2) {
+	return op1 + op2;
 };
 
-const subtract = function() {
-	
+const subtract = function(op1, op2) {
+	return op1 - op2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, num) => acc += num, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+    if(arr.length === 0) {
+      return 0;
+    }
 
+    return arr.reduce((acc, num) => acc *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+
+  for(let i = 1; i <= num; i++) {
+    result *= i
+  }
+
+  return result;
 };
 
 // Do not edit below this line
