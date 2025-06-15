5918fb244c4f9d5cff7b36506996831d39c8ac9b
Baraa <124203201+0baraa@users.noreply.github.com>
2025-06-15 12:31:50 +0100

    16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = new Date().getFullYear();
 
+  let sortedPeople = people.sort((a, b) => {
+    const ageA = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+    const ageB = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+    return ageB - ageA;
+  });
+
+  return sortedPeople[0];
 };
 
 // Do not edit below this line
