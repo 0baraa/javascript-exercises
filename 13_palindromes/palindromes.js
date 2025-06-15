7d10504c242d714974a66f6482244add70f9bfc2
Baraa <124203201+0baraa@users.noreply.github.com>
2025-06-15 12:03:37 +0100

    13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = str => {
+  const validChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const cleaned = str
+    .toLowerCase()
+    .split('')
+    .filter(char => validChars.includes(char))
+    .join('');
+  
+  return cleaned === cleaned.split('').reverse().join('');
 };
 
 // Do not edit below this line
