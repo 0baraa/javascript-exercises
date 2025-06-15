91cb4ef9eb9e5e2ad5249800a4ca22a3ea77f0b4
Baraa <124203201+0baraa@users.noreply.github.com>
2025-06-15 12:23:21 +0100

    14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let first = 0;
+  let second = 1;
+  let fibonacci;
+
+  if(num === 1) {
+    return 0;
+  } else if(num === 2) {
+    return 1;
+  }
+
+  for(let i = 3; i <= num; i++) {
+    fibonacci = first + second;
+    first = second;
+    second = fibonacci;
+  }
+
+  return fibonacci;
 
 };
 
